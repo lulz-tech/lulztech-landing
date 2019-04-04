4b1673d3492423f7134b3c4f9ed189b69ebad300
Alex Mubarakshin <muban96@mail.ru>
2019-04-04 18:40:52 +0300

    🎉 Sap! Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Memster landing
+
+## Installation
+
+```bash
+$ git clone https://github.com/AlexMubarakshin/memster-landing.git
+$ cd ./memster-landing
+
+$ npm i
+
+$ npm start
+
+# Visit localhost:3000
+```
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import './styles/App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Memster</h1>
+        </header>
+        <p className="App-intro">
+          <p>Kek</p>
+          <a target="__blank" href="https://t.me/memser_bot">Try now</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
