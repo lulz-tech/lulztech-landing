8132cb1481a2ddacb29825f84285e11a7969020a
Alex Mubarakshin <muban96@mail.ru>
2019-04-05 15:27:20 +0300

    👨🏿‍ Add man

--- src/components/boi/boi.tsx
+++ src/components/boi/boi.tsx
@@ -2,19 +2,32 @@ import * as React from "react";
 
 import boi from "../../assets/images/boiiiii.png";
 import cat from "../../assets/images/cat.png";
+import man from "../../assets/images/man.png";
 
 import "./boi.styles.css";
-
 interface IBoiProps { }
 
-interface IBoiState { }
+interface IBoiState {
+    currentPic: string;
+}
 
 export class Boi extends React.Component<IBoiProps, IBoiState> {
+    constructor(props: IBoiProps) {
+        super(props);
+
+        const rand = Math.random();
+
+        const currentPic = rand > 0.7 ? cat : man;
+        this.state = {
+            currentPic
+        };
+    }
 
     public render() {
+        const { currentPic } = this.state;
         return (
             <div id="boi-background">
-                <img className="cat" src={cat}></img>
+                <img className="cat" src={currentPic}></img>
                 <img className="boi" src={boi}></img>
             </div>
         );
