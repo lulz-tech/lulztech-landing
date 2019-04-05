3eece20a4dde9f5a0544f6ba6fc978b1ae1b8897
Alex Mubarakshin <muban96@mail.ru>
2019-04-05 11:19:10 +0300

    💦 Home layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import './styles/App.css';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Memster</h1>
-        </header>
-        <p className="App-intro">
-          <p>Kek</p>
-          <a target="__blank" href="https://t.me/memser_bot">Try now</a>
-        </p>
+      <div className="wrapper">
+        <div className="background" />
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Memster</h1>
+          </header>
+          <p className="App-intro">
+            <p>💦 Давай, пробуй 💦</p>
+            <a target="__blank" href="https://t.me/memser_bot">Тыц</a>
+          </p>
+        </div>
       </div>
     );
   }
